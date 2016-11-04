676eb91a93811e5b3da9b884690f5012b9fcb612
masterUNG <phrombutr@gmail.com>
2016-11-04 09:55:24 +0700

    get Intent to menu farmer

--- app/src/main/java/anuson/komkid/permitgeographypro/Menu_farmer.java
+++ app/src/main/java/anuson/komkid/permitgeographypro/Menu_farmer.java
@@ -1,44 +1,55 @@
 package anuson.komkid.permitgeographypro;
 
-import android.app.ActionBar;
-import android.app.Activity;
 import android.app.TabActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TabHost;
 
 /**
  * Created by komkrid on 28/9/2559.
  */
 @SuppressWarnings("deprecation")
-public class Menu_farmer extends TabActivity{
-        protected void onCreate(Bundle savedInstanceState) {
+public class Menu_farmer extends TabActivity {
+
+    //Explicit
+    private String[] userLoginStrings;
+
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_farmer);
 
-            TabHost tabHost = (TabHost)findViewById(android.R.id.tabhost);
+        TabHost tabHost = (TabHost) findViewById(android.R.id.tabhost);
 
-            TabHost.TabSpec tab1 = tabHost.newTabSpec("ABA UM");
-            TabHost.TabSpec tab2 = tabHost.newTabSpec("ABA DOIS");
-            TabHost.TabSpec tab3 = tabHost.newTabSpec("ABA TRES");
+        TabHost.TabSpec tab1 = tabHost.newTabSpec("ABA UM");
+        TabHost.TabSpec tab2 = tabHost.newTabSpec("ABA DOIS");
+        TabHost.TabSpec tab3 = tabHost.newTabSpec("ABA TRES");
 
-            tab1.setIndicator("หน้าแรก");
-            //tab1.setIndicator("",getResources().getDrawable(R.drawable.));
-            tab1.setContent(new Intent(this, Menu_farmer_1.class));
+        //Get Value from Intent
+        userLoginStrings = getIntent().getStringArrayExtra("Login");
 
-            tab2.setIndicator("ลงประกาศ");
-            //tab2.setIndicator("",getResources().getDrawable(R.mipmap.ic_launcher));
-            tab2.setContent(new Intent(this, Menu_farmer_2.class));
-
-            tab3.setIndicator("รายการประกาศ");
-            //tab3.setIndicator("",getResources().getDrawable(R.mipmap.ic_launcher));
-            tab3.setContent(new Intent(this, Menu_farmer_3.class));
-
-            tabHost.addTab(tab1);
-            tabHost.addTab(tab2);
-            tabHost.addTab(tab3);
+        //Check userLogin
+        for (int i=0;i<userLoginStrings.length;i++) {
+            Log.d("4novV1", "userLogin[" + i + "] = " + userLoginStrings[i]);
+        }   // for
 
 
+        tab1.setIndicator("หน้าแรก");
+        //tab1.setIndicator("",getResources().getDrawable(R.drawable.));
+        tab1.setContent(new Intent(this, Menu_farmer_1.class));
 
-        }
+        tab2.setIndicator("ลงประกาศ");
+        //tab2.setIndicator("",getResources().getDrawable(R.mipmap.ic_launcher));
+        tab2.setContent(new Intent(this, Menu_farmer_2.class));
+
+        tab3.setIndicator("รายการประกาศ");
+        //tab3.setIndicator("",getResources().getDrawable(R.mipmap.ic_launcher));
+        tab3.setContent(new Intent(this, Menu_farmer_3.class));
+
+        tabHost.addTab(tab1);
+        tabHost.addTab(tab2);
+        tabHost.addTab(tab3);
+
+
+    }
 }
